e154d2b4ab64555cfe6ef558b1eed792b8f33f90
kavathalekomal <kavathalekomal@gmail.com>
2022-07-15 11:05:00 +0530

    updated the binary search file

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -28,6 +28,7 @@ int BinarySearch(int arr[], int n, int key)
 int main()
 {
     int n;
+    cout << "enter number of elements of array" << endl;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
