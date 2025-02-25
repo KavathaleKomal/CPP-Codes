a7274c51553929cb9df5e635bad29ba5576bd3f7
KavathaleKomal <91792447+KavathaleKomal@users.noreply.github.com>
2025-02-25 20:26:47 +0530

    Add files via upload

---
+++ headers_list.cpp
@@ -0,0 +1,52 @@
+C:\Users\COV2KOR\AppData\Roaming\Code\User\snippets\cpp.json
+//STL
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+//Algorithm
+#include <algorithm>
+
+//strings
+#include <string>
+
+//maths
+#include <cmath>
+#include <complex>    // Complex numbers
+#include <random>     // Random numbers
+#include <numeric>    // Numeric operations like accumulate
+
+//i/o
+#include <iostream>
+#include <fstream>
+
+//template
+#include <iterator>
+#include <functional>
+
+//utilities
+#include <utility>
+#include <tuple>
+
+//concurrency
+#include <thread>
+#include <mutex>
+#include <future>
+
+//misc
+#include <bitset>
+#include <regex>
+#include <chrono> //time functions
+#include <array>
+
+//c-style compatibility headers
+<cstdio> (for stdio.h)
+<cstdlib> (for stdlib.h)
+<cstring> (for string.h)
+<cmath> (for math.h)
